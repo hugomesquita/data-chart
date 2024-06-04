d22120d0b4a950cef60e04da4ed9c4430133357a
Hugo Mesquita <hugomesquitaweb@gmail.com>
2024-06-03 22:09:49 -0400

    chart horizontalbar

--- src/HorizontalBarChart.js
+++ src/HorizontalBarChart.js
@@ -1,45 +1,60 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, Cell } from 'recharts'; // Import Cell from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Cell, ResponsiveContainer } from 'recharts'; // Import Cell from 'recharts'
 
 const HorizontalBarChart = ({ data, onDataClick, selectedLabel }) => {
+  // Grouping data by 'name' field
+  const groupedData = data.reduce((acc, curr) => {
+    if (!acc[curr.name]) {
+      acc[curr.name] = { name: curr.name, value: 0, label: curr.label };
+    }
+    acc[curr.name].value += curr.value;
+    return acc;
+  }, {});
+
+  const groupedChartData = Object.values(groupedData);
+
+
   return (
-    <BarChart
-      width={600}
-      height={300}
-      data={data}
-      layout="vertical"
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      onClick={(event) => {
-        if (event && event.activePayload && event.activePayload.length > 0) {
-          onDataClick(event.activePayload[0].payload.name);
-        }
-      }}
-    >
-      <XAxis type="number" />
-      <YAxis dataKey="name" type="category" />
-      <Tooltip />
-      <Legend />
-      <Bar 
-        dataKey="value" 
-        fill="#82ca9d" 
-        name="Falhas" 
-        isAnimationActive={false} 
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart
+        width={600}
+        height={300}
+        data={groupedChartData}
+        layout="vertical"
+        margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
         onClick={(event) => {
           if (event && event.activePayload && event.activePayload.length > 0) {
             onDataClick(event.activePayload[0].payload.name);
           }
         }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type="number" domain={[0, 400]} />
+        <YAxis dataKey="name" type="category" />
+        <Tooltip />
+        <Legend />
+        <Bar 
+          dataKey="value" 
+          fill="#82ca9d" 
+          name="Falhas" 
+          isAnimationActive={false} 
+          onClick={(event) => {
+            if (event && event.activePayload && event.activePayload.length > 0) {
+              onDataClick(event.activePayload[0].payload.name);
+            }
+          }}
         >
-        {
-          data.map((entry, index) => (
-            <Cell 
-              key={`cell-${index}`} 
-              fill={entry.name === selectedLabel ? '#ff7300' : '#82ca9d'} 
-            />
-          ))
-        }
-      </Bar>
-    </BarChart>
+          {
+            data.map((entry, index) => (
+              <Cell 
+                key={`cell-${index}`} 
+                fill={entry.name === selectedLabel ? '#287f4a' : '#82ca9d'} 
+              />
+            ))
+          }
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
   );
 };
 
--- src/LineChartComponent.js
+++ src/LineChartComponent.js
@@ -10,13 +10,18 @@ import {
   ResponsiveContainer,
   Brush,
 } from 'recharts';
+import { format } from 'date-fns';
 
 const LineChartComponent = ({ data }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
       <LineChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
+        <XAxis 
+            dataKey="date" 
+            tick={{ fontSize: 12 }}
+            tickFormatter={(date) => format(new Date(date), 'dd/MM') +1 }
+        />
         <YAxis domain={[0, 40]} />
         <Tooltip />
         <Legend />
@@ -26,7 +31,13 @@ const LineChartComponent = ({ data }) => {
           stroke="#8884d8"
           name="Falhas ao longo do tempo"
         />
-        <Brush dataKey="date" height={30} stroke="#8884d8" />
+        <Brush 
+            dataKey="date" 
+            height={30} 
+            stroke="#8884d8"
+            // startIndex={1}
+            tickFormatter={(date) => format(new Date(date), 'dd/MM')}
+        />
       </LineChart>
     </ResponsiveContainer>
   );
