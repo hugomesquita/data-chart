c963bce0deb94007fe193ddeaac9495748db5cf6
Hugo Mesquita <hugomesquitaweb@gmail.com>
2024-06-03 20:15:35 -0400

    customize line chart component

--- src/LineChartComponent.js
+++ src/LineChartComponent.js
@@ -7,28 +7,28 @@ import {
   Legend,
   XAxis,
   YAxis,
+  ResponsiveContainer,
+  Brush,
 } from 'recharts';
 
 const LineChartComponent = ({ data }) => {
   return (
-    <LineChart
-      width={600}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="date" />
-      <YAxis domain={[0, 40]} />
-      <Tooltip />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="value"
-        stroke="#8884d8"
-        name="Falhas ao longo do tempo"
-      />
-    </LineChart>
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis domain={[0, 40]} />
+        <Tooltip />
+        <Legend />
+        <Line
+          type="monotone"
+          dataKey="value"
+          stroke="#8884d8"
+          name="Falhas ao longo do tempo"
+        />
+        <Brush dataKey="date" height={30} stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
   );
 };
 
